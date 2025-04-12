3cd3fc03e64b953998f35fd78720241a4da1a3fe
Dibyakanta Barik <dibyakanta.barik@daffodilsw.com>
2025-04-12 21:25:17 +0530

    Google oauth integration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -32,6 +32,9 @@ const Login = () => {
       setError(error.response?.data || "Something went wrong");
     }
   };
+  const handleGoogleLogin = async () => {
+    window.location.href = BASE_URL + "/google/login";
+  };
 
   return (
     <div className="flex justify-center my-10">
@@ -39,7 +42,7 @@ const Login = () => {
         <div className="card-body">
           <h2 className="card-title justify-center">Login</h2>
           <div>
-            <fieldset className="fieldset">
+            <fieldset className="fieldset ">
               <legend className="fieldset-legend ">Email Id</legend>
               <input
                 type="text"
@@ -62,8 +65,8 @@ const Login = () => {
             </fieldset>
           </div>
           {error && <p className="text-red-500">Error: {error}</p>}
-          <div className="card-actions justify-center">
-            <button className="btn btn-primary" onClick={handleLogin}>
+          <div className="card-actions justify-center ">
+            <button className="btn btn-primary w-1/2 " onClick={handleLogin}>
               Login
             </button>
           </div>
@@ -73,6 +76,41 @@ const Login = () => {
               Signup here!!!
             </Link>
           </p>
+          <div className="card-actions justify-center">
+            <button
+              className="btn bg-white text-black border-[#e5e5e5]"
+              onClick={handleGoogleLogin}
+            >
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Login with Google
+            </button>
+          </div>
         </div>
       </div>
     </div>
